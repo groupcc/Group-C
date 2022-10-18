bdb9f279928a8cbc0dd6117efe97bbbd65a8a14c
SEUNGWON98 <98tjtmddnjs@Naver.com>
2022-10-18 09:42:21 +0900

    responsive

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express =require('express');
+const { render } = require("ejs");
+const express = require("express");
 
 const app = express();
 const PORT = 9000;
 
-app.set('view engine', 'ejs')
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/static', express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
 
+app.get("/", function (request, response) {
+  response.render("index");
+});
 
-app.get('/', function (request, response) {
-    response.send('Hello Team C')
-})
-
-app.listen(PORT, function() {
-    console.log(`Listening on port ${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });
