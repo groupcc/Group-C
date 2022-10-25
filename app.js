59a19ea5da5d5667cd7bc0316b5098d65bd1e8ec
SEUNGWON98 <98tjtmddnjs@Naver.com>
2022-10-25 19:42:48 +0900

    window.open

--- app.js
+++ app.js
@@ -9,7 +9,85 @@ app.use("/views", express.static(__dirname + "/views"));
 app.use("/static", express.static(__dirname + "/static"));
 
 app.get("/", function (request, response) {
-  response.render("universal");
+  response.render("worldcontinents");
+});
+
+app.get("/china", function (request, response) {
+  response.render("china");
+});
+
+app.get("/japan", function (request, response) {
+  response.render("japan");
+});
+
+app.get("/tailand", function (request, response) {
+  response.render("tailand");
+});
+
+app.get("/vietnam", function (request, response) {
+  response.render("vietnam");
+});
+
+app.get("/eygpt", function (request, response) {
+  response.render("eygpt");
+});
+
+app.get("/molicious", function (request, response) {
+  response.render("molicious");
+});
+
+app.get("/moroco", function (request, response) {
+  response.render("moroco");
+});
+
+app.get("/southafrica", function (request, response) {
+  response.render("southafrica");
+});
+
+app.get("/canada", function (request, response) {
+  response.render("canada");
+});
+
+app.get("/usa", function (request, response) {
+  response.render("usa");
+});
+app.get("/australia", function (request, response) {
+  response.render("australia");
+});
+
+app.get("/newzealand", function (request, response) {
+  response.render("newzealand");
+});
+
+app.get("/swiss", function (request, response) {
+  response.render("swiss");
+});
+
+app.get("/spain", function (request, response) {
+  response.render("spain");
+});
+
+app.get("/netherland", function (request, response) {
+  response.render("netherland");
+});
+
+app.get("/finland", function (request, response) {
+  response.render("finland");
+});
+
+app.get("/chille", function (request, response) {
+  response.render("chille");
+});
+
+app.get("/brazil", function (request, response) {
+  response.render("brazil");
+});
+app.get("/cuba", function (request, response) {
+  response.render("cuba");
+});
+
+app.get("/mexico", function (request, response) {
+  response.render("mexico");
 });
 
 app.listen(PORT, function () {
