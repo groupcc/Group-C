14207284d2ac290783096e89c4588dd3d80e9a0e
SEUNGWON98 <98tjtmddnjs@Naver.com>
2022-10-27 15:52:15 +0900

    승원

--- static/js/main6.js
+++ static/js/main6.js
@@ -1,9 +1,17 @@
-const bgImgs = ['mexico', 'bangkok', 'spain', 'australia', 'manhattan', 'egypt'];
+const bgImgs = [
+  'mexico',
+  'bangkok',
+  'spain',
+  'australia',
+  'manhattan',
+  'egypt',
+];
 const bg = document.querySelector('#slide3 > img');
 let count = 1;
-setInterval(function() {
-    for (let i =0; i<bgImgs.length; i++){
-        bg.setAttribute('src', `/static/image/${bgImgs[count % 6]}.jpg`)};
-        count = count + 1;
-        console.log(count);
+setInterval(function () {
+  for (let i = 0; i < bgImgs.length; i++) {
+    bg.setAttribute('src', `/static/img/main6/${bgImgs[count % 6]}.jpg`);
+  }
+  count = count + 1;
+  console.log(count);
 }, 3000);
